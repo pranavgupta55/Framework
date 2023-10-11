d4bb026a664aef80c1b486d0397557bc681714e7
Pranav <pxg082007@gmail.com>
2023-10-10 22:52:26 -0500

    Fonts and text handling stuff

---
+++ fontDict.py
@@ -0,0 +1,23 @@
+import pygame
+pygame.init()
+
+fonts = {"regular20": pygame.font.Font("fonts/Montserrat-Regular.ttf", 20),
+         "bold20": pygame.font.Font("fonts/Montserrat-Bold.ttf", 20),
+         "thin20": pygame.font.Font("fonts/Montserrat-Thin.ttf", 20),
+         "light20": pygame.font.Font("fonts/Montserrat-ExtraLight.ttf", 20),
+         "regular30": pygame.font.Font("fonts/Montserrat-Regular.ttf", 30),
+         "bold30": pygame.font.Font("fonts/Montserrat-Bold.ttf", 30),
+         "thin30": pygame.font.Font("fonts/Montserrat-Thin.ttf", 30),
+         "light30": pygame.font.Font("fonts/Montserrat-ExtraLight.ttf", 30),
+         "regular40": pygame.font.Font("fonts/Montserrat-Regular.ttf", 40),
+         "bold40": pygame.font.Font("fonts/Montserrat-Bold.ttf", 40),
+         "thin40": pygame.font.Font("fonts/Montserrat-Thin.ttf", 40),
+         "light40": pygame.font.Font("fonts/Montserrat-ExtraLight.ttf", 40),
+         "regular60": pygame.font.Font("fonts/Montserrat-Regular.ttf", 60),
+         "bold60": pygame.font.Font("fonts/Montserrat-Bold.ttf", 60),
+         "thin60": pygame.font.Font("fonts/Montserrat-Thin.ttf", 60),
+         "light60": pygame.font.Font("fonts/Montserrat-ExtraLight.ttf", 60),
+         "regular80": pygame.font.Font("fonts/Montserrat-Regular.ttf", 80),
+         "bold80": pygame.font.Font("fonts/Montserrat-Bold.ttf", 80),
+         "thin80": pygame.font.Font("fonts/Montserrat-Thin.ttf", 80),
+         "light80": pygame.font.Font("fonts/Montserrat-ExtraLight.ttf", 80)}
