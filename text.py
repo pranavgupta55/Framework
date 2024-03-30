27b46da883e7a6e41a506299c528e3dcb458d1bd
Pranav <pxg082007@gmail.com>
2024-03-30 15:23:04 -0500

    New getFontSizeWithWrap function

--- text.py
+++ text.py
@@ -10,6 +10,36 @@ def simpleText(screen_, color_, font_, x_, y_, text_):
     screen_.blit(font_.render(text_, True, color_), (x_, y_))
 
 
+def getFontSizeWithWrap(font_, text_, max_len):
+    out = []
+    complete_row = ""
+    if pygame.font.Font.size(font_, text_)[0] > max_len:
+        all_words = []
+        word = ""
+        complete_row = ""
+        for i, char in enumerate(text_):
+            if char != " ":
+                word += char
+            else:
+                all_words.append(word + " ")
+                word = ""
+            if i + 1 == len(text_):
+                all_words.append(word)
+        for i, word in enumerate(all_words):
+            if pygame.font.Font.size(font_, word)[0] + pygame.font.Font.size(font_, complete_row)[0] > max_len:
+                out.append(complete_row)
+                complete_row = "" + word
+            else:
+                complete_row += word
+            if i + 1 == len(all_words):
+                complete_row += word
+    else:
+        complete_row += text_
+    out.append(complete_row)
+    largest_len = max([pygame.font.Font.size(font_, line)[0] for line in out])
+    return [len(out), largest_len]
+
+
 def draw_text(screen_, color_, font_, x_, y_, text_, color2_=None, shadow_size_=0, wrap_=False, max_len=None, centered_=False):
     if wrap_:
         if pygame.font.Font.size(font_, text_)[0] > max_len:
