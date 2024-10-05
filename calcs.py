6c19366c5b0d94fefd72255f034f3972f4fd0ca4
Pranav Gupta <85249689+PGMaster1234@users.noreply.github.com>
2024-10-04 22:08:10 -0500

    Routine Update

--- calcs.py
+++ calcs.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def distance(point1, point2):
-    return math.sqrt(math.pow(math.fabs(point1[0] - point2[0]), 2) + math.pow(math.fabs(point1[1] - point2[1]), 2))
+    return ((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2) ** 0.5
 
 
 def ang(start_pos, end_pos):
